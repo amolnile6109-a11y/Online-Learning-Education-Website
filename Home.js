2d89fa9f81f59fb3719d12a12c1ab7970a94858e
amolnile6109-coder <amolnile6109@gmail.com>
2025-10-10 13:59:36 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,7 @@
+// Get the "Get Started" button
+const getStartedBtn = document.getElementById("getStartedBtn");
+
+// When clicked, go to the courses page
+getStartedBtn.addEventListener("click", () => {
+  window.location.href = "course.html";
+});
