e8e259ab3538bc7b5040e3cf6a500d5101b2fe62
amolnile6109-coder <amolnile6109@gmail.com>
2025-10-13 20:32:20 +0530

    Delete Home.js

--- Home.js
+++
@@ -1,7 +0,0 @@
-// Get the "Get Started" button
-const getStartedBtn = document.getElementById("getStartedBtn");
-
-// When clicked, go to the courses page
-getStartedBtn.addEventListener("click", () => {
-  window.location.href = "course.html";
-});
